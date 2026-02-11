bca264b67b09b84f9d05fd38631903354e412357
kfireew <kfir.magniv@gmail.com>
2026-02-11 14:28:05 +0200

    oops

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -3,11 +3,12 @@ import { connectDB } from '../db';
 import { conf } from '../utils/config';
 import { server } from '../graphql/server';
 import { koaMiddleware } from '@as-integrations/koa';
+import { logger } from '../utils';
 
 (async () => {
   await connectDB();
   await server.start();
   app.use(koaMiddleware(server));
   const port = parseInt(conf.get('port'));
-  app.listen(port, () => console.log(`🚀 Server listening on port ${port}`));
+  app.listen(port, () => logger.info(`🚀 Server listening on port ${port}`));
 })();
